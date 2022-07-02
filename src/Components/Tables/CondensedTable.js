0d066807e463f93f611b0aa14247e8561a3862ba
EmreMertA <emremakdag55@gmail.com>
2022-07-02 12:48:17 +0300

    Update CondensedTable.js

--- src/Components/Tables/CondensedTable.js
+++ src/Components/Tables/CondensedTable.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const CondensedTable = () => {
+  const users = useSelector((state) => state.users.value);
+
   return (
     <div className='col-lg-6'>
       <div className='panel'>
@@ -19,69 +22,17 @@ const CondensedTable = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
-                  <a href='#fakelink' className='btn-link'>
-                    Order #53451
-                  </a>
-                </td>
-                <td>Scott S. Calabrese</td>
-                <td>$24.98</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#fakelink' className='btn-link'>
-                    Order #53452
-                  </a>
-                </td>
-                <td>Teresa L. Doe</td>
-                <td>$564.00</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#fakelink' className='btn-link'>
-                    Order #53453
-                  </a>
-                </td>
-                <td>Steve N. Horton</td>
-                <td>$58.87</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#fakelink' className='btn-link'>
-                    Order #53454
-                  </a>
-                </td>
-                <td>Charles S Boyle</td>
-                <td>$97.50</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#fakelink' className='btn-link'>
-                    Order #53455
-                  </a>
-                </td>
-                <td>Lucy Doe</td>
-                <td>$12.79</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#fakelink' className='btn-link'>
-                    Order #53456
-                  </a>
-                </td>
-                <td>Michael Bunr</td>
-                <td>$249.99</td>
-              </tr>
-              <tr>
-                <td>
-                  <a href='#fakelink' className='btn-link'>
-                    Order #53451
-                  </a>
-                </td>
-                <td>Scott S. Calabrese</td>
-                <td>$24.98</td>
-              </tr>
+              {users.map((user) => (
+                <tr>
+                  <td>
+                    <a href='#fakelink' className='btn-link'>
+                      Order #{user.id}
+                    </a>
+                  </td>
+                  <td>S{user.name}</td>
+                  <td>${user.amount}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
